e1c93fe30579eefdb8b0253a918bbae3e55113e3
Rishwanth-M <mekalarishwanthkumar26@gmail.com>
2026-01-02 16:44:56 +0530

    Fix product schema and controller to match allproducts collection

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -69,7 +69,7 @@ const userSchema = new Schema(
 
     gender: {
       type: String,
-      enum: ["male", "female", "other"],
+      enum: ["Male", "Female", "other"],
       required: true,
     },
 
