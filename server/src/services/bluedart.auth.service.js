bc8734b745c46c167a2794cdb0187e636fdbd87a
Rishwanth-M <mekalarishwanthkumar26@gmail.com>
2026-01-10 22:38:13 +0530

    Fix product schema and controller to match allproducts collection

--- server/src/services/bluedart.auth.service.js
+++ server/src/services/bluedart.auth.service.js
@@ -1,27 +1,19 @@
 const axios = require("axios");
-const qs = require("querystring");
 
 const generateBlueDartToken = async () => {
-  const response = await axios.post(
-    process.env.BLUEDART_AUTH_URL,
-    qs.stringify({
-      client_id: process.env.BLUEDART_API_KEY,      // Consumer Key
-      client_secret: process.env.BLUEDART_API_SECRET,
-      grant_type: "client_credentials",
-    }),
+  const response = await axios.get(
+    "https://apigateway-sandbox.bluedart.com/in/transportation/token/v1/login",
     {
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        ClientID: process.env.BLUEDART_API_KEY,
+        clientSecret: process.env.BLUEDART_API_SECRET,
+        Accept: "application/json",
       },
-      timeout: 15000,
+      timeout: 10000,
     }
   );
 
-  if (!response.data?.access_token) {
-    throw new Error("BlueDart token not received");
-  }
-
-  return response.data.access_token;
+  return response.data.JWTToken;
 };
 
 module.exports = { generateBlueDartToken };
